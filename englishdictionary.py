e944f5fd1d1b53a967379ab47dbd574dd10484a4
penpenpoin <85094779+penpenpoin@users.noreply.github.com>
2023-05-09 00:43:28 +0900

    ver2 複数単語対応

--- englishdictionary.py
+++ englishdictionary.py
@@ -2,15 +2,19 @@ import bs4
 import requests
 
 url = "https://dictionary.cambridge.org/ja/dictionary/english/"
-
-word = input("Enter an English word")
-
-wurl = url + word
 headers_dic = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"}
 
-site = requests.get(wurl,headers=headers_dic,timeout=(7.0,7.5))
+word = input("Enter an English word")
+words = word.split()
+for word in words:
+	wurl = url + word
 
-soup = bs4.BeautifulSoup(site.content, "html.parser")
-tests = soup.find("div",class_="def ddef_d db")
+	site = requests.get(wurl,headers=headers_dic,timeout=(7.0,7.5))
 
-print(tests.text)
+	soup = bs4.BeautifulSoup(site.content, "html.parser")
+	tests = soup.find("div",class_="def ddef_d db")
+
+	if tests is None :
+		print("something went wrong")
+		continue
+	print(tests.text)
